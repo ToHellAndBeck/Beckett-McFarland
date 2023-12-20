9d4ba94313ee32c3b458cb07ae7da845e15361f2
ToHellAndBeck <beckettmcfarland@gmail.com>
2023-12-19 22:05:31 -0600

    update

--- Fun Scripts/fantasymon/fantasymon.py
+++ Fun Scripts/fantasymon/fantasymon.py
@@ -3,7 +3,30 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QComboBo
 from PyQt5.QtGui import QFont, QPixmap
 import csv
 import os
+from PyQt5.QtWidgets import QComboBox, QStyledItemDelegate, QStyleOptionViewItem, QStyle
 
+class ImageComboBox(QComboBox):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setView(ImageComboBoxView(self))
+
+class ImageComboBoxView(QStyledItemDelegate):
+    def paint(self, painter, option, index):
+        if option.state & QStyle.State_Selected:
+            # Draw a background for the selected item
+            painter.fillRect(option.rect, option.palette.highlight())
+
+        # Draw the image at the center of the item
+        pixmap = index.data(Qt.DecorationRole)
+        if pixmap:
+            painter.drawPixmap(option.rect.center() - pixmap.rect().center(), pixmap)
+
+    def sizeHint(self, option, index):
+        # Set the size of the item to match the size of the image
+        pixmap = index.data(Qt.DecorationRole)
+        if pixmap:
+            return pixmap.size()
+        return super().sizeHint(option, index)
 class PokemonSelectorApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -89,26 +112,43 @@ class PokemonSelectorApp(QWidget):
             None
         )
 
-        if selected_pokemon:
-            # Display Pokémon information
-            self.info_labels[0].setText(f"Type 1: {selected_pokemon['Type 1']}")
-            self.info_labels[1].setText(f"Type 2: {selected_pokemon['Type 2']}")
-            self.info_labels[2].setText(f"Total: {selected_pokemon['Total']}")
-            self.info_labels[3].setText(f"HP: {selected_pokemon['HP']}")
-            self.info_labels[4].setText(f"Attack: {selected_pokemon['Attack']}")
-            self.info_labels[5].setText(f"Defense: {selected_pokemon['Defense']}")
-            self.info_labels[6].setText(f"Speed: {selected_pokemon['Speed']}")
+        # Clear labels if the selected Pokémon is not found
+        if selected_pokemon is None:
+            for label in self.info_labels:
+                label.clear()
+            self.sprite_label.clear()
+            return
 
-            # Display Pokémon sprite based on the Pokémon's number
-            sprite_path = os.path.join('sprites', f"{selected_pokemon_number}.png")
-            if os.path.exists(sprite_path):
-                pixmap = QPixmap(sprite_path)
-                self.sprite_label.setPixmap(pixmap)
-            else:
-                self.sprite_label.clear()
+        # Display Pokémon information
+        self.info_labels[0].setText(f"Type 1: {selected_pokemon['Type 1']}")
+        self.info_labels[1].setText(f"Type 2: {selected_pokemon['Type 2']}")
+        self.info_labels[2].setText(f"Total: {selected_pokemon['Total']}")
+        self.info_labels[3].setText(f"HP: {selected_pokemon['HP']}")
+        self.info_labels[4].setText(f"Attack: {selected_pokemon['Attack']}")
+        self.info_labels[5].setText(f"Defense: {selected_pokemon['Defense']}")
+        self.info_labels[6].setText(f"Speed: {selected_pokemon['Speed']}")
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = PokemonSelectorApp()
-    window.show()
-    sys.exit(app.exec_())
+        # Display Pokémon sprite based on the Pokémon's number
+        sprite_path = os.path.join('sprites', f"{selected_pokemon_number}.png")
+        if os.path.exists(sprite_path):
+            pixmap = QPixmap(sprite_path)
+            self.sprite_label.setPixmap(pixmap)
+        else:
+            self.sprite_label.clear()
+
+
+def populate_type_dropdown(self):
+    # Populate the type dropdown with images
+    for type_name in self.get_unique_types():
+        pixmap = self.get_type_image(type_name)
+        self.type_dropdown.addItem(pixmap, type_name)
+
+def get_type_image(self, type_name):
+    # You need to implement a method to retrieve the image based on the type_name
+    # This can involve loading images from files or using resources
+    # For simplicity, let's assume you have images in the 'types' folder with the type_name as the file name
+    image_path = f'types/{type_name.lower()}.png'
+    if os.path.exists(image_path):
+        return QPixmap(image_path)
+    else:
+        return QPixmap()  # Return an empty pixmap if the image is not found
